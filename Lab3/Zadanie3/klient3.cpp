ae69c3a0f1bf41e9af7e7af499525c2adfdfa58c
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-03-19 00:25:40 +0100

    feat: add lab3

---
+++ Lab3/Zadanie3/klient3.cpp
@@ -0,0 +1,19 @@
+﻿#include<windows.h>
+#import "klasa.tlb" no_namespace
+
+/*
+ *  tlbexp .\klasa.dll /out:klasa.tlb
+ */
+
+int main() {
+
+	CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	IKlasa *s;
+	HRESULT rv;
+	rv = CoCreateInstance(__uuidof(Klasa), NULL, CLSCTX_ALL, __uuidof(IKlasa), (void **)&s);
+	s->Test("Testowanie, zadanie 3 ok!");
+	s->Release();
+	CoUninitialize();
+
+	return 0;
+};
---
+++ Lab3/Zadanie7-control-library/UserControl1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Zadanie7_control_library
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl1));
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(895, 475);
+            this.axWindowsMediaPlayer2.TabIndex = 0;
+            // 
+            // UserControl1
+            // 
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(895, 475);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+    }
+}
